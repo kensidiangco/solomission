1d41dc71b732b6cf7d0d5f66a6abced8a9a23597
kensidiangco@gmail.com <kensidiangco@gmail.com>
2025-07-17 20:09:49 +0800

    updating UI

--- src/app/pouch/page.tsx
+++ src/app/pouch/page.tsx
@@ -2,30 +2,50 @@ import Link from "next/link"
 
 export default function Pouch() {
   return (
-    <div className="p-100 flex flex-center justify-center">
-        <div className="flex flex-col gap-10">
-            <p className="font-bold text-center text-7xl mb-5">Pouch Inventory</p>
-            <div className="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
+    <div className="p-20 flex flex-center justify-center">
+        <div className="flex flex-col gap-5">
+            <p className="font-bold text-center text-7xl mb-2 text-stone-800">Pouch</p>
 
+            <div className="flex flex-col gap-4 p-2">
+                <div className="flex gap-4 pl-2 pr-2 justify-around">
+                    <div className="p-4 text-2xl font-bold bg-gradient-to-r from-stone-50 to-slate-100 rounded-t-xl hover:shadow-2xl text-stone-600">
+                        <p className="">Small</p>
+                        <p className="">1k</p>
+                    </div>
+                    <div className="p-4 text-2xl font-bold bg-gradient-to-r from-stone-50 to-slate-100 rounded-t-xl hover:shadow-2xl text-stone-600">
+                        <p className="">Medium</p>
+                        <p className="">1k</p>
+                    </div>
+                    <div className="p-4 text-2xl font-bold bg-gradient-to-r from-stone-50 to-slate-100 rounded-t-xl hover:shadow-2xl text-stone-600">
+                        <p className="">Large</p>
+                        <p className="">1k</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="rounded-t-xl overflow-hidden bg-gradient-to-r from-stone-50 to-slate-100 p-10">
                 <table className="table-auto">
                     <thead>
                         <tr>
-                            <th className="px-4 py-2 text-emerald-600">GETTER</th>
-                            <th className="px-4 py-2 text-emerald-600">SIZE</th>
-                            <th className="px-4 py-2 text-emerald-600">QTY</th>
-                            <th className="px-4 py-2 text-emerald-600">PURPOSE</th>
-                            <th className="px-4 py-2 text-emerald-600">GIVEN BY (OPTIONAL)</th>
-                            <th className="px-4 py-2 text-emerald-600">DATE CREATED</th>
+                            <th className="px-4 py-2 text-stone-600">GETTER</th>
+                            <th className="px-4 py-2 text-stone-600">SIZE</th>
+                            <th className="px-4 py-2 text-stone-600">QTY</th>
+                            <th className="px-4 py-2 text-stone-600">PURPOSE</th>
+                            <th className="px-4 py-2 text-stone-600">GIVEN BY (OPTIONAL)</th>
+                            <th className="px-4 py-2 text-stone-600">DATE CREATED</th>
+                            <th className="px-4 py-2 text-stone-600">STATUS</th>
+                            
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">KEN</td>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">MEDIUM</td>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">1K</td>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">FOR SELL</td>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">GOKONWEI</td>
-                            <td className="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">Sample</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">KEN</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">MEDIUM</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">1K</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">FOR SELL</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">GOKONWEI</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">SAMPLE</td>
+                            <td className="border border-stone-500 px-4 py-2 text-stone-600 font-medium">PAID</td>
                         </tr>
                     </tbody>
                 </table>
--- src/app/something/page.tsx
+++ src/app/something/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function SOMETHING() {
   return (
     <div className="p-100">
-      <p className="font-bold text-center text-7xl mb-5">SOMETHING!</p>
+      <p className="font-bold text-center text-7xl mb-5">Coming soon!</p>
       <Link href={"/"} className="font-bold text-center text-4xl">
         <p className="text-blue-400">Back!</p>
       </Link>
